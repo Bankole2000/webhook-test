89e3c514a1106df477ffb0c13c6a3ec86debbd55
Bankole Esan <techybanky@gmail.com>
2023-06-21 23:12:16 +0100

    setup

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send({
+app.get("/", async (req: Request, res: Response) => {
+  return res.status(200).send({
     message: "This works"
   });
 });
 
-app.post("/", (req: Request, res: Response) => {
-  res.status(200).send({
+app.post("/", async (req: Request, res: Response) => {
+  return res.status(200).send({
     message: "This works"
   });
 });
